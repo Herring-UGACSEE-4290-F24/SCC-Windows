9dd4044f88132e6fc6e6de2b9eca642d0e4a9227
cczipp <ccz83048@uga.edu>
2024-10-04 10:45:12 -0400

    Added comments from the Checkup

--- IF/ID/IF.v
+++ IF/ID/IF.v
@@ -36,3 +36,14 @@ module IF(clk,reset, taken, branch_address,pc,instruction)
     end
 
 endmodule
+
+// For IF:
+// Combine this with the ID module
+// No muxes in these files, they will be in their own separate files
+// Memory file is outside the SCC (instruction mem and data mem)
+
+// For ID:
+// Cases would be cleaner/better than else/if statements to turn into hardware
+// Cases also make debugging easier
+// Cannot be multiple registers in the path, single cycle computer means only one at a time
+// The register will be in the memory file or testbench (most things will be wires to transmit data)
