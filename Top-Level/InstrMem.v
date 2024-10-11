ea7c78262b67c13c41ec24f130d0f4c6e890338e
Connor Loosemore <connor.loosemore2021@gmail.com>
2024-10-10 21:37:00 -0400

    resolved merge conflict... again?

--- Top-Level/InstrMem.v
+++ Top-Level/InstrMem.v
@@ -1,10 +1,9 @@
 module InstrMem(clk, address, instruction)
 begin
-    input           clk             // clock signal
+    input               clk             // clock signal
     input wire [31:0]   address         // 32-bit instruction address
     output reg [31:0]   instruction     // instruction to be executed
 
-<<<<<<< HEAD
     //Defining instruction memory, although there is address space for 2^32 locations, here we have 255 for simplicity
     reg [31:0] memory [0:255];
 
@@ -24,8 +23,4 @@ begin
     always @(posedge clk) begin
         instruction = memory[address[7:0]];  // Memory contains 255 locations, so use bits [7:0] from the address
     end
-=======
-    output [31:0]   instruction     // instruction to be executed
-
->>>>>>> b944aacfa373753400439313bd12a09f687036eb
 end
