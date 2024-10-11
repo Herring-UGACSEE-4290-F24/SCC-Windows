2411d46bea9ceed520cb51c41856b1d36c3595ba
Connor Loosemore <connor.loosemore2021@gmail.com>
2024-10-11 01:51:35 -0400

    Updated register file to have correct behavior surrounding read/write ports, etc.
    Should not have to be touched until determination is made about having additional reserved registers or any other major changes to the architecture

--- Top-Level/RegFile.v
+++ Top-Level/RegFile.v
@@ -1,19 +1,41 @@
-module RegFile(clk, address, register)
-begin
-    input               clk             // clock signal
-    input wire [2:0]   address         // 32-bit instruction address
-    output reg [31:0]   register     // instruction to be executed
+module RegFile(clk, r_addr_0, r_addr_1, w_addr, w_enable, w_select, w_alu, w_id, r_val_0, r_val_1)
+
+    /===========================================  I/O  ===================================================/
+    input                    clk         //Clock signal
+    input [2:0]              r_addr_0    //Read port 0 address
+    input [2:0]              r_addr_1    //Read port 1 address
+    input [2:0]              w_addr      //Write port address
+    input                    w_enable    //Enables writing to regs (active high)
+    input                    w_select    //Mux select for ALU/ID writing to reg files, 0 = ALU, 1 = ID
+    input [31:0]             w_alu       //Write data to reg from ALU stage
+    input [31:0]             w_id        //Write data to reg from ID stage
+
+    output wire [31:0]       r_val_0     //Read port 0 output value
+    output wire [31:0]       r_val_1     //Read port 1 output value
+    /=====================================================================================================/
 
     //Defining register file, there are 2^3 = 8 registers, each 32 bits wide
     reg [31:0] registers [0:7];
 
-    initial begin
-        // Initialize all registers to zero
-        for (int i = 0; i < 7; i = i + 1) registers[i] = 32'h00000000;
+    //Assigns read port 0/1 output the value of the register pointed to by read port 0/1 input address, when the address is updated.
+    //(Reads are performed asynchronously)
+    always @(r_addr_0) begin
+        assign r_val_0 <= registers[r_addr_0[2:0]]; //Read port 0
+    end
+    
+    always @(r_addr_1) begin
+        assign r_val_1 <= registers[r_addr_1[2:0]]; //Read port 1
     end
 
-    // Read instruction from memory based on the address input
+    // Write value from w_alu or w_id, dependent on control line w_select, to register pointed to by w_addr, if w_enable is active.
+    //(Writes are performed synchronously) 
     always @(posedge clk) begin
-        register = registers[address[2:0]];  // 8 register locations, accessed by 3 address bits
+        if (w_enable) begin 
+            if (w_select)
+                registers[w_addr[2:0]] = w_id;
+            else
+                registers[w_addr[2:0]] = w_alu;
+        end
     end
-end
+
+endmodule
