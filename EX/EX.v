120b411b0f02257899d69900c92a8580253f9703
cczipp <ccz83048@uga.edu>
2024-10-14 13:26:10 -0400

    Fixing the same error a second time isn't as bad as figuring it out the first time. Now EXE has been compiled.

--- EX/EX.v
+++ EX/EX.v
@@ -1,10 +1,9 @@
-module EX(1LD, Special_encoding, 2LD, ALU_OC, B_cond, dest_reg, pointer_reg, op_1_reg_value, op_2_reg_value, immediate, offset, flags, ALU_results )
+module EX(First_LD, Special_encoding, Second_LD, ALU_OC, B_cond, dest_reg, pointer_reg, op_1_reg_value, op_2_reg_value, immediate, offset, flags, ALU_results);
 
-
-    //just the outputs of decode 
-    input wire [1:0]        1LD;                    
+    // Just the outputs of decode 
+    input wire [1:0]        First_LD;                    
     input wire              Special_encoding;
-    input wire [3:0]        2LD;
+    input wire [3:0]        Second_LD;
     input wire [2:0]        ALU_OC;
     input wire [3:0]        B_cond;
     input wire [2:0]        dest_reg;
@@ -15,103 +14,90 @@ module EX(1LD, Special_encoding, 2LD, ALU_OC, B_cond, dest_reg, pointer_reg, op_
     input wire [15:0]       offset;
     input wire [3:0]        flags;
 
-    output wire [15:0]      ALU_results
+    output wire [15:0]      ALU_results;
+    reg [15:0]              result;
 
-    
-    // 3 Commands to implement Add_Immediate, Add_Regs, and NOP
-    // module ALU
+    // Assign the result to ALU_results
+    assign ALU_results = result;
 
-    if(Special_encoding)            // Ironically Special Encoding high means ALU so this is ALU FUNCTIONS
-        begin
-        case (ALU_OC[2:0])
-            3'b001:                  // ADD Control Command
-               // Retrieve dest_reg 
-               // Retrieve op_1_reg
-               
-                if(1LD[0] == 1'b0)   // If first level decode lsb = 1 ---> Immediate is being used
-                    begin
-                       assign  ALU_results = op_1_reg_value + immediate;
-                    end
-                else                 // Else aka first level decode lsb = 0 ---> Register is being used
-                    begin
-                       assign  ALU_results = op_1_reg_value + op_2_reg_value;
-                    end
-            3'b010:                  // SUB Control Command
-                if(1LD[0] == 1'b0)   // If first level decode lsb = 1 ---> Immediate is being used
-                    begin
-                       assign  ALU_results = op_1_reg_value - immediate;
-                    end
-                else                 // Else aka first level decode lsb = 0 ---> Register is being used
-                    begin
-                       assign  ALU_results = op_1_reg_value - op_2_reg_value;
-                    end
-            3'b011:                  // AND Control Command
-                if(1LD[0] == 1'b0)   // If first level decode lsb = 1 ---> Immediate is being used
-                    begin
-                       assign ALU_results = op_1_reg_value & immediate;
-                    end
-                else                 // Else aka first level decode lsb = 0 ---> Register is being used
-                    begin
-                       assign ALU_results = op_1_reg_value & op_2_reg_value;
-                    end
-            3'b100:                  // OR Control Command
-                if(1LD[0] == 1'b0)   // If first level decode lsb = 1 ---> Immediate is being used
-                    begin
-                       assign ALU_results = op_1_reg_value | immediate;
-                    end
-                else                 // Else aka first level decode lsb = 0 ---> Register is being used
-                    begin
-                       assign ALU_results = op_1_reg_value | op_2_reg_value;
-                    end
-            3'b101:                  // XOR Control Command
-                if(1LD[0] == 1'b0)   // If first level decode lsb = 1 ---> Immediate is being used
-                    begin
-                       assign ALU_results = op_1_reg_value ^ immediate;
-                    end
-                else                 // Else aka first level decode lsb = 0 ---> Register is being used
-                    begin
-                       assign ALU_results = op_1_reg_value ^ op_2_reg_value;
-                    end
-            3'b110:                  // NOT Control Command
-                if(1LD[0] == 1'b0)   // If first level decode lsb = 1 ---> Immediate is being used
-                    begin
-                       assign ALU_results = ~op_1_reg_value; 
-                    end
-                else                 // Else aka first level decode lsb = 0 ---> Register is being used
-                    begin
-                       assign ALU_results = ~op_1_reg_value;
-                    end
-        endcase
-        end
-    else if (1LD[1:0] = 2'b00)        //NON ALU FUNCTIONS With REG   ****LSL DOCUMENTATION IS WRONG LSL OP is 27-25 [1,0,0] not the same as mov****
-        begin
-        case (ALU_OC[2:0])
-             3'b000:                  // MOV Command
-            
-             3'b001:                  // MOVT commands
-
-             3'b100:                  //LSL
-
-             3'b101:                  //LSR
-             
-             3'b010:                  //CLR
-
-             3'b011:                  //SET
-
-        end
-    else                            //Branching and System calls deafult
-        begin
+    always @(*) begin
+        if (Special_encoding) begin
+            // Ironically Special Encoding high means ALU, so these are ALU FUNCTIONS
             case (ALU_OC[2:0])
-             3'b000:                  // Branch command   b
-            
-             3'b001:                  // Branch conditional
-
-             3'b010:                  // Branch command br
-
-             deafult:                  // NOP
-
+                3'b001: begin // ADD Control Command
+                    if (First_LD[0] == 1'b0) // Immediate is being used
+                        result = op_1_reg_value + immediate;
+                    else // Register is being used
+                        result = op_1_reg_value + op_2_reg_value;
+                end
+                3'b010: begin // SUB Control Command
+                    if (First_LD[0] == 1'b0)
+                        result = op_1_reg_value - immediate;
+                    else
+                        result = op_1_reg_value - op_2_reg_value;
+                end
+                3'b011: begin // AND Control Command
+                    if (First_LD[0] == 1'b0)
+                        result = op_1_reg_value & immediate;
+                    else
+                        result = op_1_reg_value & op_2_reg_value;
+                end
+                3'b100: begin // OR Control Command
+                    if (First_LD[0] == 1'b0)
+                        result = op_1_reg_value | immediate;
+                    else
+                        result = op_1_reg_value | op_2_reg_value;
+                end
+                3'b101: begin // XOR Control Command
+                    if (First_LD[0] == 1'b0)
+                        result = op_1_reg_value ^ immediate;
+                    else
+                        result = op_1_reg_value ^ op_2_reg_value;
+                end
+                3'b110: begin // NOT Control Command
+                    result = ~op_1_reg_value;
+                end
+                default: result = 16'b0; // Default case for unspecified ALU_OC values
+            endcase
         end
-
-
-    endmodule   
-    
+        else if (First_LD == 2'b00) begin
+            // Non-ALU functions with REG
+            case (ALU_OC[2:0])
+                3'b000: begin
+                    // Implement MOV operation
+                end
+                3'b001: begin
+                    // Implement MOVT command
+                end
+                3'b100: begin
+                    // Implement LSL (Logical Shift Left)
+                end
+                3'b101: begin
+                    // Implement LSR (Logical Shift Right)
+                end
+                3'b010: begin
+                    // Implement CLR (Clear)
+                end
+                3'b011: begin
+                    // Implement SET
+                end
+                default: result = 16'b0;
+            endcase
+        end
+        else begin
+            // Branching and System calls default
+            case (ALU_OC[2:0])
+                3'b000: begin
+                    // Implement branch command 'b'
+                end
+                3'b001: begin
+                    // Implement branch conditional
+                end
+                3'b010: begin
+                    // Implement branch command 'br'
+                end
+                default: result = 16'b0; // NOP
+            endcase
+        end
+    end
+endmodule
