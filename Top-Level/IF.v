af2e327562341bdba20eab2d18b9ed124eaf7957
Connor Loosemore <connor.loosemore2021@gmail.com>
2024-11-03 21:55:46 -0500

    Cleaned up formatting in IF

--- Top-Level/IF.v
+++ Top-Level/IF.v
@@ -3,13 +3,13 @@
 module IF(clk, reset, prefetch, pc, instruction);
 
     //===========================================  I/O  ===================================================//
-    input                   clk;                   // main clock 
-    input                   reset;                 // resets pc to known state
-    input wire [31:0]       prefetch;              // instruction pre-fetched one ahead
-    output reg [31:0]       pc;                    // address of next instruction in memory
-    output reg [31:0]       instruction;           // fetched instruction
+    input                   clk;                    // main clock 
+    input                   reset;                  // resets pc to known state
+    input wire [31:0]       prefetch;               // instruction pre-fetched one ahead
+    output reg [31:0]       pc;                     // address of next instruction in memory
+    output reg [31:0]       instruction;            // fetched instruction
 
-    reg  [31:0]             br_immediate;          // Non-conditional branch immediate
+    reg  [31:0]             br_immediate;           // Non-conditional branch immediate
     //=====================================================================================================//
 
     // Instruction fetching logic
@@ -20,18 +20,18 @@ module IF(clk, reset, prefetch, pc, instruction);
 
     always @(*)
         begin
-            if (prefetch[31:25] == 7'b1100000)         // Checking if instruction is unconditional branch
+            if (prefetch[31:25] == 7'b1100000)      // Checking if instruction is unconditional branch
                 begin
                 #5
-                br_immediate = prefetch[15:0];              // Extracting immediate value from instruction
+                br_immediate = prefetch[15:0];      // Extracting immediate value from instruction
                 if (br_immediate[15] == 1'b0)                   
                     begin
-                    pc = pc + br_immediate;                 // Incrementing PC by positive offset
+                    pc = pc + br_immediate;         // Incrementing PC by positive offset
                     end
                 else
                     begin
-                    br_immediate[31:16] = 16'hFFFF;        // Sign-extending immediate value to 32 bits
-                    pc = pc + br_immediate;                // Decrementing PC by negative offset
+                    br_immediate[31:16] = 16'hFFFF; // Sign-extending immediate value to 32 bits
+                    pc = pc + br_immediate;         // Decrementing PC by negative offset
                     end
             end
     end
@@ -41,10 +41,10 @@ module IF(clk, reset, prefetch, pc, instruction);
         instruction <= prefetch;
 
         if (reset) begin
-            pc = 32'h0000;                              // Resets PC if reset is high
+            pc = 32'h0000;                          // Resets PC if reset is high
         end 
         else begin
-            pc = pc + 32'h0004;                         // Default case: Increment PC by four
+            pc = pc + 32'h0004;                     // Default case: Increment PC by four
         end
     end
 endmodule
